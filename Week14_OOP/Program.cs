725a3c3c28d0a0a8ccb42c43364115531334129a
KadriGit <kadri.viira@tktk.ee>
2024-12-12 22:58:03 +0200

    final code

--- Week14_OOP/Program.cs
+++ Week14_OOP/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Dog GoodBoy = new Dog("Good Boy");
+
+Console.WriteLine($"Name: {GoodBoy.Name}; Level og the happiness: {GoodBoy.LevelOfHappiness}");
+
+GoodBoy.Bark();
+Console.WriteLine($"Name: {GoodBoy.Name}; Level og the happiness: {GoodBoy.LevelOfHappiness}");
+
+
+while (GoodBoy.LevelOfHappiness < 5)
+{
+    GoodBoy.Bark();
+}
+GoodBoy.Wag();
+
+
+class Dog
+{
+    public string Name;
+    public int LevelOfHappiness;
+
+    public Dog(string Name)
+    {
+        Name = Name;
+        LevelOfHappiness = 0;
+        Console.WriteLine($"{Name} is your friend now.");
+    }
+
+    public void Bark()
+    {
+        LevelOfHappiness++;
+        Console.WriteLine("Whoof-whoof!");
+    }
+
+    public void Wag()
+    {
+        LevelOfHappiness++;
+        Console.WriteLine($"{Name} starts wagging it's tail");
+        Console.WriteLine("Whoosh-whoosh");
+    }
+}
